34d27c1382713c69b97c66bd55eaf78f08b16a03
Jon Fackrell <jon.fackrell@gmail.com>
2020-09-17 00:33:48 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# subject-specialists
+# Subject Specialists Demo
+
+The purpose of this project was to create a webpage with a list of subject specialist librarians. 
+
+- [x] List librarian's picture, name, and subject specialties (each librarian is displayed once)
+- [x] Librarians can be sorted alphabetically and reverse alphabetically by first and last name
+- [x] Effort was made to make page accessible 
+- [x] Page is responsive 
+
+My initial goal was to create a standalone script that could be reused; however, due to time constraints there is still quite a bit of work to make this a reality.
+
+The subject specialist page was created using vanilla javascript in conjuction with [Tailwind CSS](https://tailwindcss.com/), [Axios](https://www.npmjs.com/package/axios), [Underscore](https://underscorejs.org/), and [Handlebars](https://handlebarsjs.com/).
+
+## Usage
+
+```javascript
+<script type="text/javascript">
+    var ss = ss || {};
+    var dc = document.createElement('script');
+    dc.type = 'text/javascript'; dc.async = true;
+    dc.src = 'build/js/subject-specialists.min.js';
+    document.getElementsByTagName("body")[0].appendChild(dc);
+</script>
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/subject-specialists.js
@@ -0,0 +1,123 @@
+window.ss = {
+
+    template: null,
+
+    container: null,
+
+    people: [],
+
+    init: function(){
+
+        ss.setTemplate();
+
+        axios.get('subject-specialists.json')
+            .then(function (response) {
+                ss.container = document.getElementById('subject-specialists');
+                ss.data = response.data;
+                ss.people = ss.removeDuplicates(ss.data);
+                ss.people = ss.sortBy(ss.people, 'first_name', 'az');
+                ss.updateDisplay();
+            })
+            .catch(function (error) {
+                alert(error);
+            })
+            .then(function () {
+                ss.registerEvents();
+            });
+
+    },
+
+    registerEvents: function () {
+        var search = document.getElementById('search_field');
+        search.addEventListener("keyup", ss.search);
+        search.addEventListener("search", ss.search);
+
+        var sort = document.getElementById('sort');
+        sort.addEventListener("change", function(){
+            var [property, order] = event.target.value.split(":")
+            ss.people = ss.sortBy(ss.people, property, order);
+            ss.updateDisplay();
+        });
+    },
+
+    updateDisplay: function (){
+        ss.container.innerHTML = '';
+        ss.people.forEach(function (person){
+            ss.container.insertAdjacentHTML('beforeend', ss.template(person));
+        });
+    },
+
+    setTemplate: function (){
+        Handlebars.registerHelper('url', function () {
+            if(this.last_name == 'Kaur'){
+                this.full_name = 'Aman Kaur';
+            }
+            return 'https://www.library.upenn.edu/people/staff/' + this.full_name.split(' ').join('-');
+        })
+
+        ss.template = Handlebars.compile(
+      '   <li class="subject-specialist" data-name="{{ full_name }}" data-subject="{{ job_title }}">\n' +
+            '        <article class="space-y-4">\n' +
+            '            <div class="relative pb-2/3">\n' +
+            '                <img class="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://www.library.upenn.edu/{{ thumbnail }}" alt="Profile picture of {{ full_name }}">\n' +
+            '            </div>\n' +
+            '\n' +
+            '            <div class="space-y-2">\n' +
+            '                <div class="text-lg text-gray-900 leading-6 font-medium space-y-1">\n' +
+            '                    <h4><a href="{{ url }}">{{ full_name }}</a></h4>\n' +
+            '                    <p class="text-gray-600">{{{ subject_specialty }}}</p>\n' +
+            '                </div>\n' +
+            '            <div class="text-lg leading-7">\n' +
+            '                <p class="text-gray-500">{{{ office }}}</p>\n' +
+            '            </div>' +
+            '            <div class="mt-5 max-w-md flex justify-between md:mt-8">\n' +
+            '               {{#if email}}<a href="mailto:{{ email }}" class="text-blue-900 hover:text-gray-500 transition ease-in-out duration-150 pr-4">\n' +
+            '                   {{ email }}\n' +
+            '               </a>\n{{/if}}' +
+            '               {{#if phone}}<a href="tel:{{ phone }}" class="text-blue-900 hover:text-gray-500 transition ease-in-out duration-150">\n' +
+            '                   {{ phone }}\n' +
+            '               </a>\n{{/if}}' +
+            '               </div>\n' +
+            '            </div>\n' +
+            '        </article>\n' +
+            '    </li>');
+    },
+
+    removeDuplicates: function(people){
+        return _.unique(people, true, function(person){ return person.full_name});
+    },
+
+    sortBy: function(people, property, direction = 'az'){
+        people = _.sortBy(people, property);
+        if(direction == 'za'){
+            people = people.reverse();
+        }
+        return people;
+    },
+
+    search: function(event){
+        if (event.isComposing || event.keyCode === 229) {
+            return;
+        }
+        var peopleContainers = Array.prototype.slice.call(document.getElementsByClassName('subject-specialist'));
+        peopleContainers.forEach(function(person){
+            if(ss.compare(person, ['name', 'subject'], event.target.value)){
+                person.style.display = "block";
+            }else{
+                person.style.display = "none";
+            }
+        });
+    },
+
+    compare: function (person, properties, value) {
+        var contains_match = false;
+        properties.forEach(function (property) {
+            if(person.dataset[property].toUpperCase().indexOf(value.toUpperCase()) >= 0){
+                contains_match = true;
+            }
+        });
+        return contains_match;
+    }
+};
+
+ss.init();
